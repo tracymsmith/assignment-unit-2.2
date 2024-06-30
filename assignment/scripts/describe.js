b125376d493e60fff67023e7bce796624f23a758
Tracy Smith <tmhsllc@icloud.com>
2024-06-30 14:58:59 -0500

    activity describe and correct various code

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,15 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
+
+// (re: line 41) - We make a variable called name and set it to 'Dane' as a name.
 // 
-// 
-// 
-//
+// (re: lines 43-47) - We write a conditional statement to evaluate the value of a name.
+// If the value of the name exactly (===) matches 'Mary' (e.g., is TRUE, e.g., if the
+// condition is met), then the code executes (e.g., console logs or prints) the
+// conditional statement 'Hi, Mary!'; else if the value of the name does not
+// match 'Mary' (e.g., is FALSE, e.g., if the condition is not met), then
+// the code executes (e.g., console logs or prints) the conditional statement 'How do you do?'.
 
 //CODE
 /*
@@ -46,9 +51,20 @@ if (name === 'Mary') {
 
 // 2. WRITE YOUR DESCRIPTION HERE
 // 
+// (re: line 71) -  We make an undefined variable called secret.
 // 
-// 
+// (re: line 73) - We make a number variable called code and set it to the number 123 as a code.
 //
+// (re: lines 75-78)  - If the value of the variable code exactly (===) matches the nunber 123 (e.g., is TRUE, e.g., if the
+// condition is met), then the code block is run and the undefined variable 'secret' is now defined as the string 'super'.
+// The code block continues to run and the variable code is redefined as its value multiplied by 2 (e.g., 123 times (*) 2) = 246.
+// 
+// (re: lines 80-82) - If the value of the variable 'code' is greater than (>) 250 (e.g., is TRUE, e.g., if the condition is met), 
+// then the code block is run and the undefined variable 'secret' is now defined as the string 'duper'.
+// The value of the variable 'code' is not greater than 250 as it is now 246 therefore the variable 'secret' remains defined
+// as the string 'duper'.
+// 
+// (re: line 84) - The code executes (console logs or prints) the still defined value of 'secret' as the string 'super'.
 
 //CODE
 /*
@@ -70,9 +86,19 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
+
+// (re: lines 106-118) - We make a boolean variable called 'isStudent' and set it to 'true'; we make a number variable
+// called 'age' and set it to the number 34; we make a number variable called 'zip' and set it to the number 55407.
 // 
-// 
-// 
+// (re: lines 102-110) - If the value of isStudent is exactly true and if the value of zip is greater than 80000, then
+// the code block is run and the code executes (console logs or prints) the conditional statement 'You're a student on
+// the West Coast!'. Both of these conditions were not met therefore the conditional statemnent would not be executed.
+// If the value if isStudent is false or (only one of the conditions needs to be me) the age is less than 30 then the code
+// block is run and the code executes (console logs or prints) the conditional statement 'What are your hobbies?'. Neither
+// of the conditions were met therefore the conditional statement would not be executed. If the value of isStudent is exactly
+// true (which it is), then the code block is run and executes the conditional statement 'Welcome to Prime!' If the variable isStudent
+// was false (which it is not but rather is true) then the code block is run and executes the conditional statement
+// 'How about the weather?' (which is did not execute).
 //
 
 //CODE
@@ -130,9 +156,15 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+// FIX - colorOne should be 'blue' and colorTwo should be 'red' per instructions
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
+
+// FIX - mix is true therefore both colors become purple so colorTwo needs to be added as follows:
+// if (mix === true) {
+//  colorOne = 'purple'; colorTwo = 'purple';
+// }
 
 if (mix === true) {
   colorOne = 'purple';
@@ -144,6 +176,15 @@ if (mix === true) {
 // -- they are so we console.log 'throw away the food!'
 
 /*
+
+// FIX - instructions state 'and' for both conditionals but the code is written 'or' so
+// the '||' needs to be changed to '&&'. Also need to remove 'const' in front of the
+// variable 'time' and change it to 'let time = 4;' as the instructions did not
+// state that the variable 'time' cannot be changed. The instructions say 'and' so both
+// conditions of the 2 variables need to be true in order to console log the statement
+// 'throw away the food!'. They are not both true therefore the statement will not
+// console log.
+
 let temp = 40;
 const time = 4;
 
@@ -155,6 +196,18 @@ if (temp > 39 || time >= 4) {
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
+
+// FIX - instructions say to check if age is greater than or equal to minAge,
+// however code is written if minAge is less than or equal to age. This part needs to change to
+// 'if(age >= minAge)' because the way it is written in error would deny someone entry if their age = 21.
+// New code should be:
+// 
+// if(age >= minAge) {
+//   console.log('enter');
+//   }
+// else{
+//   console.log('no entry')
+// }
 
 /*
 let age = 21;
